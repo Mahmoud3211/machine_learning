d2012dd5033c23b9ac7779679652b03504b6c5a4
Mahmoud3211 <mahmoudnada5997@gmail.com>
2020-10-05 17:31:11 +0200

    wall thickness detected

--- 5_power_pipes.py
+++ 5_power_pipes.py
@@ -72,14 +72,16 @@ def filter_with_point(lines, y=200, tx=10):
 
     return np.array(lines)[index_array]
         
-def check_thickness(full_lines):
+def check_thickness(full_lines, diff_thresh=5, edge_thresh=3):
     y1 = 300
     y2 = 500
     y3 = 700
     y4 = 900
-    
+
     selected_coor = []
-    for group in np.array_split(full_lines, len(full_lines) // 4)
+    check_array = []
+    for group in np.array_split(full_lines, len(full_lines) // 4):
+        
         for line in group:
             slope, intercept = get_slop_intercept(line)
             slope = 0 if slope is None else slope
@@ -90,12 +92,24 @@ def check_thickness(full_lines):
             x3 = (y3 - intercept) // slope if slope != 0 else 0
             x4 = (y4 - intercept) // slope if slope != 0 else 0
 
-            selected_coor.append((x1, x2, x3, x4))
+            selected_coor.append(np.array([x1, x2, x3, x4]))
+        dist1 = np.abs(selected_coor[0] - selected_coor[1])
+        dist2 = np.abs(selected_coor[2] - selected_coor[3])
+        print(dist1, dist2)
+
+        edge_diff = np.all(np.abs(dist1 - dist2) >= edge_thresh)
+        # right_edge = np.all(dist1 > thresh)
+        # left_edge = np.all(dist2 > thresh)
+
+        check_array.append(edge_diff)
+    return check_array
+
+
 
 results_path = os.path.join(os.getcwd(), 'results')
 test_path = os.path.join(os.getcwd(), 'test')
 
-image = cv2.imread(os.path.join(test_path, 'wt2.bmp'))
+image = cv2.imread(os.path.join(test_path, 'a.bmp'))
 cv2.imshow('original', cv2.resize(image, (800, 600)))
 # cv2.imwrite(os.path.join(results_path, 'original.jpg'), cv2.resize(image, (800, 600)))
 
@@ -145,8 +159,16 @@ for x1, y1, x2, y2  in filtered_lines:
         for x, y in zip(xs, ys):
             cv2.circle(clineImage, (x,y), 2, (255,0,0), 2)
 
-print(np.array_split(filtered_lines, len(filtered_lines)//4))
+try:
+    wt_pos = check_thickness(filtered_lines, 5).index(True) + 1
+    cv2.putText(clineImage, f'there is wall thickness error in tupe number {wt_pos}', (0,50), cv2.FONT_HERSHEY_SIMPLEX , 1, (0,0,255), 2)
+except ValueError:
+    cv2.putText(clineImage, 'No wall thickness error detected', (0,50), cv2.FONT_HERSHEY_SIMPLEX , 1, (0,0,255), 2)
+
 cv2.imshow('res filtered', cv2.resize(clineImage, (800, 600)))
+cv2.imwrite(os.path.join(results_path, 'wall_thickness 2.jpg'), cv2.resize(clineImage, (800, 600)))
+
+# print(check_thickness(filtered_lines, 5).index(True))
 # cv2.imwrite(os.path.join(results_path, 'res filtered.jpg'), cv2.resize(clineImage, (800, 600)))
 
 
